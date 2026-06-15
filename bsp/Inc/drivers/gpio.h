9d16d20af1c0925f1bdd6867fabbaceb88205837
keating <keatingsane@outlook.com>
2026-06-15 02:39:16 -0400

    refactor(bsp): add shared gpio config helpers

--- bsp/Inc/drivers/gpio.h
+++ bsp/Inc/drivers/gpio.h
@@ -6,6 +6,10 @@
 #ifndef GPIO_H
 #define GPIO_H
 
+#include <stdint.h>
+
+#include "stm32f446xx.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -19,6 +23,37 @@ void led_init(void);
  * @brief  toggle pa5 (ld2) output data register
  */
 void led_toggle(void);
+
+// alternate-function pin setup, shared by the peripheral drivers
+
+typedef enum {
+    GPIO_PUSH_PULL = 0,
+    GPIO_OPEN_DRAIN = 1,
+} gpio_otype_t;
+
+typedef enum {
+    GPIO_SPEED_LOW = 0,
+    GPIO_SPEED_MEDIUM = 1,
+    GPIO_SPEED_HIGH = 2,
+    GPIO_SPEED_VERY_HIGH = 3,
+} gpio_speed_t;
+
+/**
+ * @brief  enable a gpio port's ahb1 clock - call before configuring its pins
+ * @param  port  the gpio port (GPIOA, GPIOB, ...)
+ */
+void gpio_enable_port(GPIO_TypeDef* port);
+
+/**
+ * @brief  configure one pin as an alternate function
+ * @param  port   the gpio port
+ * @param  pin    pin number, 0-15
+ * @param  af     alternate-function number, 0-15 (see the datasheet af table)
+ * @param  otype  push-pull or open-drain
+ * @param  speed  output slew-rate setting
+ */
+void gpio_config_af(GPIO_TypeDef* port, uint32_t pin, uint8_t af, gpio_otype_t otype,
+                    gpio_speed_t speed);
 
 #ifdef __cplusplus
 }
--- bsp/Src/drivers/gpio.c
+++ bsp/Src/drivers/gpio.c
@@ -9,8 +9,7 @@
 
 void led_init(void) {
     // ld2 on port A
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-    (void)RCC->AHB1ENR;
+    gpio_enable_port(GPIOA);
 
     // set mode to output for pa5
     GPIOA->MODER &= ~GPIO_MODER_MODE5_Msk;
@@ -25,4 +24,26 @@ void led_toggle(void) {
     } else {
         GPIOA->BSRR = (1U << 5U);  // currently low -> set pa5
     }
+}
+
+void gpio_enable_port(GPIO_TypeDef* port) {
+    // the ports sit 0x400 apart and the ahb1enr enable bits run 0,1,2,...
+    uint32_t idx = ((uint32_t)port - GPIOA_BASE) / 0x400UL;
+    RCC->AHB1ENR |= (1UL << idx);
+    (void)RCC->AHB1ENR;  // readback lets the clock settle before the port is touched
+}
+
+void gpio_config_af(GPIO_TypeDef* port, uint32_t pin, uint8_t af, gpio_otype_t otype,
+                    gpio_speed_t speed) {
+    port->MODER &= ~(0x3UL << (pin * 2U));
+    port->MODER |= (0x2UL << (pin * 2U));  // alternate function
+
+    port->OTYPER &= ~(0x1UL << pin);
+    port->OTYPER |= ((uint32_t)otype << pin);
+
+    port->OSPEEDR &= ~(0x3UL << (pin * 2U));
+    port->OSPEEDR |= ((uint32_t)speed << (pin * 2U));
+
+    port->AFR[pin >> 3U] &= ~(0xFUL << ((pin & 0x7U) * 4U));
+    port->AFR[pin >> 3U] |= ((uint32_t)af << ((pin & 0x7U) * 4U));
 }
